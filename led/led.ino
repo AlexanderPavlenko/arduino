0e68505d6b1f23e29963c8c7837c5f6189d5bb64
AlexanderPavlenko <AlexanderPavlenko@users.noreply.github.com>
2018-06-29 00:02:26 +0300

    Adds all_at_once mode

--- led/led.ino
+++ led/led.ino
@@ -16,13 +16,18 @@ void setup() {
   delay(500); // "power-up safety"
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
-  currentPalette = RainbowColors_p;
-  // currentPalette = PartyColors_p;
+  // currentPalette = RainbowColors_p;
+  currentPalette = PartyColors_p;
   currentBlending = LINEARBLEND;
 }
 
 
 void loop() {
+  morph();
+  // all_at_once();
+}
+
+void morph() {
   static uint8_t colorIndex = 0;
   static uint8_t brightness = 255;
 
@@ -35,6 +40,14 @@ void loop() {
     FastLED.show();
     delay(1000 / UPDATES_PER_SECOND);
   }
-  
+
   colorIndex = colorIndex + 1;
+}
+
+void all_at_once() {
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette(currentPalette, i, 255, currentBlending);
+  }
+  FastLED.show();
+  exit(0);
 }
