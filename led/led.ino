c9dcada6d27021b876d9887b4dd3c0ef5889de10
AlexanderPavlenko <AlexanderPavlenko@users.noreply.github.com>
2018-11-25 19:34:13 +0300

    Adds WS2812FX lib modes

--- led/led.ino
+++ led/led.ino
@@ -1,29 +1,58 @@
-#include <FastLED.h>
+#include <FastLED.h>  // https://github.com/FastLED/FastLED
+#include <WS2812FX.h> // https://github.com/kitesurfer1404/WS2812FX
 
 #define LED_TYPE    WS2811
 #define LED_PIN     8
+#define LED_COUNT   300
 #define COLOR_ORDER GRB
-#define NUM_LEDS    300
 #define BRIGHTNESS  255
 
-CRGB leds[NUM_LEDS];
+CRGB leds[LED_COUNT];
 CRGBPalette16 palette;
 TBlendType    blending;
 
+WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 void setup() {
   delay(500); // "power-up safety"
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(BRIGHTNESS);
-  // palette = RainbowColors_p;
-  palette = PartyColors_p;
-  blending = LINEARBLEND;
+  setupFastLED();
+//  setupWS2812FX();
 }
 
 void loop() {
-  morph();
-  // stream();
-  // all_at_once();
+//  FastLED, pick one:
+//  morph();
+  stream();
+//  all_at_once();
+  
+//  or WS2812FX:
+//  ws2812fx.service();
 }
+
+void setupFastLED() {
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, LED_COUNT).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+  palette = RainbowColors_p;
+//  palette = PartyColors_p;
+  blending = LINEARBLEND;
+}
+
+void setupWS2812FX() {
+  ws2812fx.init();
+  ws2812fx.setBrightness(BRIGHTNESS);
+
+//  A)
+  ws2812fx.setMode(FX_MODE_FIREWORKS_RANDOM);
+  ws2812fx.setSpeed(50000);
+
+//  B)
+//  ws2812fx.setSegment(0, 0,   99,  FX_MODE_LARSON_SCANNER, RED,   20000, false);
+//  ws2812fx.setSegment(1, 100, 199, FX_MODE_LARSON_SCANNER, GREEN, 10000, false);
+//  ws2812fx.setSegment(2, 200, 299, FX_MODE_LARSON_SCANNER, BLUE,  5000, false);
+  
+  ws2812fx.start();
+}
+
 
 #define MORPH_SEAMLESS 10
 #define MORPH_DELAY 55
@@ -31,7 +60,7 @@ void morph() {
   static uint8_t colorIndex = 0;
 
   for(int mod = 0; mod < MORPH_SEAMLESS; mod++) {
-    for(int i = 0; i < NUM_LEDS; i++) {
+    for(int i = 0; i < LED_COUNT; i++) {
       if(i % MORPH_SEAMLESS == mod) {
         leds[i] = ColorFromPalette(palette, colorIndex, BRIGHTNESS, blending);
       }
@@ -47,7 +76,7 @@ void morph() {
 void stream(){
   static uint8_t offset = 0;
 
-  for(int i = 0; i < NUM_LEDS; i++) {
+  for(int i = 0; i < LED_COUNT; i++) {
     leds[i] = ColorFromPalette(palette, i - offset, BRIGHTNESS, blending);
   }
   FastLED.show();
@@ -57,7 +86,7 @@ void stream(){
 }
 
 void all_at_once() {
-  for(int i = 0; i < NUM_LEDS; i++) {
+  for(int i = 0; i < LED_COUNT; i++) {
     leds[i] = ColorFromPalette(palette, i, BRIGHTNESS, blending);
   }
   FastLED.show();
